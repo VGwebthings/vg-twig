88243396ded26651d35f230bfd89aa1895123c6f
Panos Paganis <mallinanga@gmail.com>
2014-09-21 15:23:52 +0300

    Don't need this

--- index.php
+++ index.php
@@ -5,7 +5,6 @@ if ( !class_exists( 'Timber' ) ) {
 }
 $context            = Timber::get_context();
 $context[ 'posts' ] = Timber::get_posts();
-$context[ 'foo' ]   = 'bar';
 $templates          = array( 'index.twig' );
 if ( is_home() ) {
     array_unshift( $templates, 'home.twig' );
