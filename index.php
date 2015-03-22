6986d3e7f518ade012c0551ba97aff2f1685de9c
Panos Paganis <mallinanga@gmail.com>
2015-03-22 12:13:46 +0200

    Don't output anything

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
 if ( ! class_exists( 'Timber' ) ) {
-    echo 'Timber not activated. Make sure you activate the plugin in <a href="/wp-admin/plugins.php#timber">/wp-admin/plugins.php</a>';
-
     return;
 }
 //$start               = TimberHelper::start_timer();
