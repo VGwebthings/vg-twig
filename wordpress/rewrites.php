a53a07d689e3d7751548232ce0615e1b0bf69fd3
Panos Paganis <mallinanga@gmail.com>
2016-06-05 22:10:34 +0300

    Rewrites

--- wordpress/rewrites.php
+++ wordpress/rewrites.php
@@ -11,3 +11,41 @@ add_filter('date_rewrite_rules', function ($rules) {
 add_filter('search_rewrite_rules', function ($rules) {
     return [];
 });
+add_filter('rewrite_rules_array', function ($rules) {
+    foreach ($rules as $rule => $rewrite) {
+        /*
+        if (preg_match('/.*category_name=/', $rewrite)) {
+            unset($rules[$rule]);
+        }
+        if (preg_match('/.*tag=/', $rewrite)) {
+            unset($rules[$rule]);
+        }
+        */
+        if (preg_match('/.*post_format=/', $rewrite)) {
+            unset($rules[$rule]);
+        }
+        if (preg_match('/.*tb=1/', $rewrite)) {
+            unset($rules[$rule]);
+        }
+        if (preg_match('/.*cpage=/', $rewrite)) {
+            unset($rules[$rule]);
+        }
+        if (preg_match('/.*attachment=/', $rewrite)) {
+            unset($rules[$rule]);
+        }
+    }
+
+    return $rules;
+});
+
+remove_action('wp_head', 'wp_oembed_add_discovery_links');
+remove_action('wp_head', 'wp_oembed_add_host_js');
+add_filter('rewrite_rules_array', function ($rules) {
+    foreach ($rules as $rule => $rewrite) {
+        if (preg_match('/.*embed=/', $rewrite)) {
+            unset($rules[$rule]);
+        }
+    }
+
+    return $rules;
+});
