c91c3106db309f3878de9bcc8c6ad707a828a1af
Panos Paganis <mallinanga@gmail.com>
2016-06-05 21:33:18 +0300

    Route example

---
+++ app/routes.php
@@ -0,0 +1,9 @@
+<?php
+Routes::map('experiments', function ($params) {
+    if ( ! current_user_can('manage_options')) {
+        wp_redirect(home_url());
+        exit;
+    }
+    var_dump($params);
+    //Routes::load('template-experiments.php', $params, false, 200);
+});
