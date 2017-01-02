88d9481c72f73ca097ac6547193d6fc110b9185f
Panos Paganis <mallinanga@gmail.com>
2017-01-02 08:14:21 +0200

    Gravity Forms related functionality

--- gravity-forms/gravity-forms.php
+++ gravity-forms/gravity-forms.php
@@ -1,16 +1,16 @@
 <?php
 add_filter('gform_disable_view_counter', '__return_true');
 add_filter('gform_init_scripts_footer', '__return_true');
-add_filter('gform_cdata_open', function ($content = '') {
-    $content = 'window.addEventListener( "load", function() { ';
-
-    return $content;
-});
-add_filter('gform_cdata_close', function ($content = '') {
-    $content = ' }, false );';
-
-    return $content;
-});
+//add_filter('gform_cdata_open', function ($content = '') {
+//    $content = 'document.addEventListener( "DOMContentLoaded", function() { ';
+//
+//    return $content;
+//});
+//add_filter('gform_cdata_close', function ($content = '') {
+//    $content = ' }, false );';
+//
+//    return $content;
+//});
 add_action('wp_ajax_nopriv_vg_get_form', 'vg_get_form');
 add_action('wp_ajax_vg_get_form', 'vg_get_form');
 function vg_get_form()
